ad1379e727007899d4ea5374a395980401aca6c9
BlightG <birhane.gulilat@gmial.com>
2022-08-11 12:08:17 -0700

    version k

--- printf.c
+++ printf.c
@@ -10,36 +10,103 @@
 int _printf(const char *format, ...)
 {
 	va_list arg;
-	int i, strlength;
+	int i = 0, strlength, numPercent = 0, loopCounter = 0;
+	char * currStr;
+	bool inPercent = false;
 
 	va_start(arg, format);
 	strlength = strlen(format);
-	for (i = 0; i < strlength; i++)
+	printf("strlen: %d\n", strlength); 
+	for (loopCounter = 0; loopCounter < strlength; i++, loopCounter++)
 	{
-		if (format[i] == '%')
+		/**if (format[loopCounter] == '%' && format[loopCounter + 1] == '%')
 		{
+			loopCounter += 2;
+			numPercent++;
+			i = _putchar('%', i);
+		}**/
+		if (format[loopCounter] == '%' && format[loopCounter + 1] != '%')
+		{
+			loopCounter++;
 			i++;
-			if (format[i] == 'c')
+			numPercent++;
+			if (format[loopCounter] == 'c')
+			{
 				i = _putchar(va_arg(arg, int), i);
-			else if (format[i] == 'd')
+				numPercent++;
+				loopCounter++;
+			}
+			else if (format[loopCounter] == 'd')
+			{
 				i = print_number(va_arg(arg, int), i);
-			else if (format[i] == 'u')
+				numPercent++;
+				loopCounter++;
+			}
+			else if (format[loopCounter] == 'u')
+			{
 				i = print_number((va_arg(arg, unsigned int)), i);
-			else if (format[i] == 'i')
+				numPercent++;
+				loopCounter++;
+			}
+			else if (format[loopCounter] == 'i')
+			{
 				i = print_number(va_arg(arg, int), i);
-			else if (format[i] == 's')
-				i = _puts((va_arg(arg, char*)), i);
-			else if (format[i] == 'o')
+				numPercent++;
+				loopCounter++;
+			}
+			else if (format[loopCounter] == 's')
+			{
+				currStr = va_arg(arg, char *);
+				if (currStr == "\0" || !currStr)
+					currStr = "(null)";
+				i = _puts(currStr, i);
+				if (i > strlength)
+					break;
+				strlength += strlen(currStr);
+				numPercent++;
+				loopCounter++;
+			}
+			else if (format[loopCounter] == 'o')
+			{
 				i = DecimalToOctal(va_arg(arg, unsigned int), i);
-			else if (format[i] == 'x')
+				numPercent++;
+				loopCounter++;
+			}
+			else if (format[loopCounter] == 'x')
+			{
 				i = DecimalToHex(va_arg(arg, unsigned long int), i, 1);
-			else if (format[i] == 'X')
+				numPercent++;
+				loopCounter++;
+			}
+			else if (format[loopCounter] == 'X')
+			{
 				i = DecimalToHex(va_arg(arg, unsigned long int), i, 0);
-			else if (format[i] == 'p')
+				numPercent++;
+				loopCounter++;
+			}
+			else if (format[loopCounter] == 'p')
+			{
 				i = print_pointer(va_arg(arg, void *), i);
+				numPercent++;
+				loopCounter++;
+			}
+			else
+			{
+				inPercent = false;
+				i--;
+			}
 		}
-		_putchar(format[i], i);
+		if (format[loopCounter] == '%' && format[loopCounter + 1] != '%')
+		{
+			loopCounter--;
+			continue;
+		}
+		if (!format[loopCounter])
+			break;
+		_putchar(format[loopCounter], i);
 	}
+//	_putchar('\0', i);
+	i -= numPercent - 1;
 	va_end(arg);
 	return (i);
 }
